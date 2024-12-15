09bff967bb9fc1c9db0af7c6844e6eb63c5653d1
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2024-12-15 04:14:50 +0200

    fix

--- components/layout/comparison/index.vue
+++ components/layout/comparison/index.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="px-14">
+  <section>
     <header class="text-center">
       <h2 class="text-[5rem] leading-small">сравни</h2>
     </header>
