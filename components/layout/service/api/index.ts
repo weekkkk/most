56c3f6d5bec134d51fb6c575819c599357471a4b
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2024-12-27 02:57:35 +0200

    [Конфигурация] Подготовить архитектуру проекта для работы

--- components/layout/service/api/index.ts
+++ components/layout/service/api/index.ts
@@ -1,6 +1,6 @@
 const SERVICE_API_URL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/service/"
+    ? "http://localhost:3000/most/service/"
     : "https://weekkkk.github.io/most/service";
 
 export const $service = $fetch.create({
