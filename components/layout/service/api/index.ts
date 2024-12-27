f05c58fbce3e1b920c395fb09a9b3fa5e344a84b
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2024-12-27 22:09:07 +0200

    fix

--- components/layout/service/api/index.ts
+++ components/layout/service/api/index.ts
@@ -1,6 +1,6 @@
 const SERVICE_API_URL =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:3000/most/service/"
+    ? "http://localhost:3000/service/"
     : "https://weekkkk.github.io/most/service";
 
 export const $service = $fetch.create({
