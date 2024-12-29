ffac4024c5d78d7f9b835151353ac80ee9cb74b7
KossaDenis <denis.kosssa@gmail.com>
2024-12-29 23:46:21 +0300

    Форма обратной связи

--- components/form/feedback/api/index.ts
+++ components/form/feedback/api/index.ts
@@ -1,10 +1,5 @@
-import type { FeedbackModal } from "./types";
+const FEEDBACK_API_URL = "https://most-9080.onrender.com/api/form";
 
-const SERVICE_API_URL = "https://most-9080.onrender.com/api/form";
-
-export const $service = (response: FeedbackModal) =>
-  $fetch.create({
-    baseURL: SERVICE_API_URL,
-    method: "POST",
-    body: response,
-  });
+export const $feedback = $fetch.create({
+  baseURL: FEEDBACK_API_URL,
+});
