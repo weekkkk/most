5ab2c37fc0ba58428a84865c0dd4695265a26fd9
KossaDenis <denis.kosssa@gmail.com>
2024-12-31 11:53:46 +0300

    Страницы прайслиста

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts"></script>
 
 <template>
-  <LayoutHeaderWidget />
-  <NuxtPage />
-  <LayoutFooterWidget />
+  <!-- <LayoutHeaderWidget /> -->
+  <!-- <NuxtPage /> -->
+  <PriceListPage />
+  <!-- <LayoutFooterWidget /> -->
 </template>
