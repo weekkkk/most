0c1fa468d3914949b51cddbe000dcfa8632cefcf
KossaDenis <denis.kosssa@gmail.com>
2025-01-01 20:06:42 +0300

    Форма обратной связи

--- src/widgets/filter/ui/index.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts"></script>
-<template>
-  <div>
-    <UiButton class="w-[9.2rem]">обувь</UiButton>
-    <UiButton class="w-[9.2rem]">сумки</UiButton>
-    <UiButton class="w-[9.2rem]">химчистка</UiButton>
-    <UiButton class="w-[9.2rem]">доставка</UiButton>
-  </div>
-</template>
