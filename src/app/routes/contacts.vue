15318363593be93599ed85f352d90352844c6c9a
AlexeyFrunza <tvoih8r@mail.ru>
2025-01-05 17:24:06 +0200

    добавлена страница контактов

---
+++ src/app/routes/contacts.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <ContactsPage />
+</template>
---
+++ src/pages/contacts/ui/page/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="flex flex-col items-center mt-[7.75rem]">
+    <h1 class="text-[2.5rem] leading-[2rem] w-[17.25rem] text-center md:text-[9rem] md:leading-[6rem] md:w-[89rem]">остались какие-то вопросы? обсудим</h1>
+    <div class="flex flex-col gap-[4.15rem] mt-[3rem]">
+      <div class="flex flex-col items-center gap-[1rem]">
+        <p class="text-[1rem] leading-[1.2rem] md:text-[2rem] md:leading-[2.4rem]">позвони</p>
+        <a href="tel:89261234858" class="text-[1.75rem] leading-[1.5rem] md:text-[5rem] md:leading-[5rem]">+7 926 123 4858</a>
+      </div>
+      <div class="flex flex-col gap-[1rem]">
+        <p class="text-[1rem] leading-[1.2rem] md:text-[2rem] md:leading-[2.4rem] text-center">напиши</p>
+        <ul class="flex flex-col gap-[1.5rem]">
+          <li class="flex justify-center text-[1.75rem] leading-[1.5rem] md:text-[5rem] md:leading-[5rem]">
+            <a href="https://t.me/MostClean_manager" target="_blank">telegram</a>
+          </li>
+          <li class="flex justify-center text-[1.75rem] leading-[1.5rem] md:text-[5rem] md:leading-[5rem]">
+            <a href="https://www.instagram.com/most.cleans/" target="_blank">instagram</a>
+          </li>
+          <li class="flex justify-center text-[1.75rem] leading-[1.5rem] md:text-[5rem] md:leading-[5rem]">
+            <a href="https://wa.me/79261234858" target="_blank">whatsapp</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
