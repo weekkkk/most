91a81cbe6403963cd782378d3bfc5274c04583ee
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-01-07 01:07:34 +0200

    [Химчистка] [grjXKeeW] Старницы химчистки - поправил папки

--- src/shared/ui/page-link/index.vue
+++ src/shared/ui/page-link/index.vue
@@ -2,21 +2,24 @@
 import type { UiPageLinkProps } from "./types";
 
 const props = defineProps<UiPageLinkProps>();
+
+const $btn = ref<HTMLButtonElement>();
 </script>
 
 <template>
   <li
-    class="max-md:text-[0.7rem] page_link group list-none inline-flex flex-col"
+    class="max-md:text-[0.7rem] page_link group list-none inline-flex flex-col items-start"
   >
     <button
-      class="page_link-header bg-brand-100 text-default w-full md:p-[20px] max-md:p-3 md:leading-control max-md:leading-[0.25rem] md:rounded-t-[calc(10px+1.2rem)] md:rounded-b-[calc(10px+1.2rem)] max-md:rounded-t-[0.875rem] max-md:rounded-b-[0.875rem] group-hover:rounded-b-none transition-all duration-150 group-hover:delay-0"
+      ref="$btn"
+      class="page_link-header group-hover:w-full text-left bg-brand-100 text-default md:p-[20px] max-md:p-3 md:leading-control max-md:leading-[0.25rem] md:rounded-t-[calc(10px+1.2rem)] md:rounded-b-[calc(10px+1.2rem)] max-md:rounded-t-[0.875rem] max-md:rounded-b-[0.875rem] group-hover:rounded-b-none transition-all duration-150 group-hover:delay-0"
     >
       <slot name="header" v-bind="{ label }">
         {{ label }}
       </slot>
     </button>
 
-    <div v-if="children" :class="{ 'max-h-0': absolute }">
+    <div v-if="children" class="w-full" :class="{ 'max-h-0': absolute }">
       <div
         class="page_link-item_list md:-mt-3 max-md:-mt-1 bg-brand-100 md:rounded-b-[calc(10px+1.2rem)] max-md:rounded-b-[0.875rem] max-h-0 overflow-hidden transition-all group-hover:delay-75"
       >
@@ -51,6 +54,7 @@ const props = defineProps<UiPageLinkProps>();
   transition-duration: calc(v-bind("children?.length") * 100ms + 25ms);
 }
 .page_link .page_link-header {
+  width: calc(v-bind("$btn?.offsetWidth") * 1px);
   transition-delay: calc(v-bind("children?.length") * 100ms + 25ms - 75ms);
 }
 </style>
