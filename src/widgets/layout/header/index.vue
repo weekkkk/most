5bb7eb1a01b43af1d8a81e16593a95c008584bc9
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-01-07 15:13:33 +0200

    build fix

--- src/widgets/layout/header/index.vue
+++ src/widgets/layout/header/index.vue
@@ -1,66 +1,66 @@
 <script setup lang="ts">
 const startNavItems: UiHeaderProps["startNavItems"] = [
-  // {
-  //   label: "химчистка",
-  //   to: "/",
-  // },
-  // {
-  //   label: "ремонт",
-  //   to: "/repair",
-  //   children: [
-  //     {
-  //       label: "сумки",
-  //       to: "bags",
-  //     },
-  //     {
-  //       label: "обувь",
-  //       to: "shoes",
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: "реставрация",
-  //   to: "/restoration",
-  //   children: [
-  //     {
-  //       label: "сумки",
-  //       to: "bags",
-  //     },
-  //     {
-  //       label: "обувь",
-  //       to: "shoes",
-  //     },
-  //   ],
-  // },
-  // {
-  //   label: "обувь",
-  //   to: "/complex",
-  //   children: [
-  //     {
-  //       label: "туфли",
-  //       to: "shoes",
-  //     },
-  //     {
-  //       label: "кроссовки",
-  //       to: "sneakers",
-  //     },
-  //     {
-  //       label: "ботинки",
-  //       to: "boots",
-  //     },
-  //   ],
-  // },
+  {
+    label: "химчистка",
+    to: "/",
+  },
+  {
+    label: "ремонт",
+    to: "/repair",
+    children: [
+      {
+        label: "сумки",
+        to: "bags",
+      },
+      {
+        label: "обувь",
+        to: "shoes",
+      },
+    ],
+  },
+  {
+    label: "реставрация",
+    to: "/restoration",
+    children: [
+      {
+        label: "сумки",
+        to: "bags",
+      },
+      {
+        label: "обувь",
+        to: "shoes",
+      },
+    ],
+  },
+  {
+    label: "обувь",
+    to: "/complex",
+    children: [
+      {
+        label: "туфли",
+        to: "shoes",
+      },
+      {
+        label: "кроссовки",
+        to: "sneakers",
+      },
+      {
+        label: "ботинки",
+        to: "boots",
+      },
+    ],
+  },
 ];
 
 const endNavItems: UiHeaderProps["endNavItems"] = [
-  // {
-  //   label: "доставка",
-  //   to: "/delivery",
-  // },
-  // {
-  //   label: "контакты",
-  //   to: "/contacts",
-  // },
+  {
+    label: "доставка",
+    to: "/delivery",
+  },
+  {
+    label: "контакты",
+    to: "/contacts",
+  },
 ];
 </script>
 
