29a1f8841d3d98bcebe8ac4c7b674e4959a67574
Nikita Nedelko <nikita.nedelko.job@gmail.com>
2025-01-12 00:58:47 +0200

    [o0GMtwrj] Опитимизация - Сделать заглушку на выдачу пустого результата поиском на странице "Цены"

--- src/widgets/service/ui/list/index.vue
+++ src/widgets/service/ui/list/index.vue
@@ -14,5 +14,17 @@ const items = computed(() =>
 </script>
 
 <template>
-  <ServiceList v-if="items" :items="items" />
+  <ServiceList v-if="items?.length" :items="items" />
+  <section v-else-if="items && !items.length">
+    <h2
+      :class="[
+        'text-center',
+        'text-[5rem] max-2xl:text-[4rem] max-md:text-[1.75rem]',
+        '-tracking-[calc(5rem/100)] max-2xl:-tracking-[calc(4rem/100)] max-md:-tracking-[calc(1.75rem/100)]',
+        'leading-[5rem] max-2xl:leading-[4rem] max-md:leading-[1.5rem]',
+      ]"
+    >
+      ничего не найдено
+    </h2>
+  </section>
 </template>
