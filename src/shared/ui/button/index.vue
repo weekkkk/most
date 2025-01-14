4c20c1e4856b800daf7c5037cfde31cdf17c6d8e
KossaDenis <denis.kosssa@gmail.com>
2025-01-15 00:14:39 +0300

    [owssjtcb] Поправить размер кнопок контантактов на мобилке - Изменил высоту кнопкам на мобилке

--- src/shared/ui/button/index.vue
+++ src/shared/ui/button/index.vue
@@ -28,15 +28,15 @@ const imgSrc = computed(() => `${config.app.baseURL}${props.img}`);
         'md:leading-control max-md:leading-[0.25rem]': !img,
       },
       {
-        'p-[1.5rem] md:px-[4rem] max-md:h-[4rem] max-md:px-12 md:rounded-[calc(1.5rem+(1rem/2))] max-md:rounded-[calc(1.5rem+(0.25rem/2))]':
+        'p-[1.5rem] md:px-[4rem] max-md:px-12 md:rounded-[calc(1.5rem+(1rem/2))] max-md:rounded-[calc(1.5rem+(0.25rem/2))]':
           !img && size === 'medium',
-        'md:p-[0.9rem] max-md:p-[0.4rem] max-md:h-[4rem] md:rounded-[calc(18px+(24px/2))] max-md:rounded-[calc(0.4rem+(24px/2))]':
+        'md:p-[0.9rem] max-md:p-[0.4rem] md:rounded-[calc(18px+(24px/2))] max-md:rounded-[calc(0.4rem+(24px/2))]':
           img && size === 'medium',
         'md:p-[1.5rem] max-md:p-[1.8rem] max-md:h-[4rem] md:rounded-[calc(1.5rem+(24px/2))] max-md:rounded-[calc(1.8rem+(24px/2))]':
           img && size === 'large',
         'md:px-[2rem] max-md:px-[1.25rem] max-md:h-[4rem]':
           img && size === 'large' && slots.default,
-        'md:px-[1rem] md:py-[0.5rem]  max-md:p-3 max-md:h-[4rem] md:rounded-[calc(1rem+(1rem/2))] max-md:rounded-[calc(1rem+(0.25rem/2))]':
+        'md:px-[1rem] md:py-[0.5rem] max-md:p-3 md:rounded-[calc(1rem+(1rem/2))] max-md:rounded-[calc(1rem+(0.25rem/2))]':
           !img && size === 'small',
       },
       { 'cursor-default': readonly },
