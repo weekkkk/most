2934b58cbd44669157c27b3dd61d20e3b1935e05
KossaDenis <denis.kosssa@gmail.com>
2025-01-15 01:57:29 +0300

    [owssjtcb] Правки Денис - Поправить UI блока Отзывы

--- src/entities/review/ui/list/index.vue
+++ src/entities/review/ui/list/index.vue
@@ -8,15 +8,13 @@ const itemClasses = [
   "col-start-1 col-end-2 row-start-2 row-end-3",
   "col-start-2 col-end-3 row-start-2 row-end-3",
   "col-start-3 col-end-4 row-start-1 row-end-2",
-  "col-start-4 col-end-5 row-start-1 row-end-2",
-  "col-start-4 col-end-5 row-start-2 row-end-3",
 ];
 </script>
 
 <template>
   <div
     :class="[
-      'max-md:hidden grid grid-cols-4 gap-4',
+      'max-md:hidden grid grid-cols-4 gap-[1rem]',
       'grid-rows-[22.5rem_22.5rem] max-2xl:grid-rows-[15.6rem_15.6rem]',
     ]"
   >
--- src/entities/review/ui/list/ui/item/index.vue
+++ src/entities/review/ui/list/ui/item/index.vue
@@ -7,7 +7,7 @@ const props = defineProps<ReviewListItemProps>();
 <template>
   <figure
     :class="[
-      'relative overflow-hidden flex flex-col justify-end',
+      'relative overflow-hidden flex flex-col justify-end w-[17.5rem]',
       'rounded-[3.5rem] max-2xl:rounded-[2rem] max-md:rounded-2xl',
     ]"
   >
@@ -22,19 +22,6 @@ const props = defineProps<ReviewListItemProps>();
         'pb-8 max-2xl:pb-[1.25rem]',
         'max-md:hidden',
       ]"
-    >
-      <header class="mb-2 max-2xl:text-[0.7rem]">
-        <p>{{ firstName }} {{ lastName }}</p>
-      </header>
-      <footer class="flex justify-center">
-        <a href="#">
-          <UiButton size="small">
-            <span class="max-2xl:text-[0.7rem]">
-              {{ username }}
-            </span>
-          </UiButton>
-        </a>
-      </footer>
-    </main>
+    ></main>
   </figure>
 </template>
