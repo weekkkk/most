782dd67be1ba4490fd4fdd0c0d5dc2b624abb694
Nikita Nedelko <weekkkk@users.noreply.github.com>
2025-02-10 17:13:52 +0300

    add analitic

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -169,6 +169,28 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: "ru",
       },
+      script: [
+        {
+          type: "text/javascript",
+          textContent: `(function(m,e,t,r,i,k,a){m[i]=m[i]function(){(m[i].a=m[i].a[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(99861262, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });`,
+        },
+      ],
+      noscript: [
+        {
+          innerHTML: `<div><img src="https://mc.yandex.ru/watch/99861262" style="position:absolute; left:-9999px;" alt="" /></div>`,
+        },
+      ],
     },
   },
   nitro: {
